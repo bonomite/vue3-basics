1f371c9b6a5f422fd25219a4fa6c1a16b3153fb6
Thomas Bono <thomasbono.com@gmail.com>
2022-06-14 22:30:15 -0400

    sample TMDB

---
+++ components/movie-card.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: null,
+  },
+})
+
+const data = ref(props.data)
+
+//console.log('data in card', data.value)
+</script>
+
+<template>
+  <div class="card">
+    <h3>{{ data.title }} {{ data.release_date }}</h3>
+    <img
+      :src="`https://image.tmdb.org/t/p/w220_and_h330_face${data.poster_path}`"
+      :alt="data.tagline"
+    />
+    <nuxt-link :to="data.homepage" target="_blank">
+      <Button icon="pi pi-external-link" class="p-button-rounded" />
+    </nuxt-link>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  position: relative;
+  padding: 1rem;
+  background-color: #fff;
+  border-radius: 0.25rem;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+  .p-button {
+    position: absolute;
+    left: 25px;
+    bottom: 30px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,37 @@
-<script setup></script>
+<script setup>
+const {
+  data: movies,
+  pending,
+  error,
+} = await useFetch(
+  `https://api.themoviedb.org/3/movie/now_playing?api_key=9e0e3fea734301f2135b9ab28c9d8be5&language=en-US&page=1`
+)
+console.log('movies', movies.value)
+// console.log('title', movie.value.title)
+</script>
 
 <template>
   <div>
     <section>
       <div class="content">
         <h2>Home page</h2>
-        <nuxt-link to="/about" class="nuxt-link"
-          ><Button label="About"
-        /></nuxt-link>
+        <nuxt-link to="/about" class="nuxt-link">
+          <Button label="About" />
+        </nuxt-link>
+        <div v-if="pending">loading...</div>
+        <div v-else class="all-movies">
+          <div class="grid">
+            <template
+              v-for="(movie, index) in movies.results"
+              :key="`movie${index}`"
+            >
+              <movie-card
+                class="col-12 md:col-6 lg:col-4 xl:col-3"
+                :data="movie"
+              />
+            </template>
+          </div>
+        </div>
       </div>
     </section>
   </div>
